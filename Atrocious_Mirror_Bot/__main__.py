6a6b44a45e5727a154fce70052f8caa98876384e
htdmirrorleech2 <101875871+htdmirrorleech2@users.noreply.github.com>
2022-03-19 11:44:04 +0630

    Update __main__.py

--- Atrocious_Mirror_Bot/__main__.py
+++ Atrocious_Mirror_Bot/__main__.py
@@ -44,9 +44,9 @@ def stats(update, context):
         
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Owner", "https://t.me/smexynos7870")
-    buttons.buildbutton("Cloud Drive Group", "https://t.me/joinchat/WKZqyWNHpLViMmI1")
-    buttons.buildbutton("Support Group", "https://t.me/Atrocious_Bot_Support")
+    buttons.buildbutton("Owner", "https://t.me/howtodoyt")
+    buttons.buildbutton("Mirror Leech Group", "https://t.me/htdmirrorleechgp")
+    buttons.buildbutton("Tutorial", "https://youtu.be/fuR3k1EkwI4")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
@@ -55,7 +55,7 @@ I can mirror all your links to Google Drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 For any question join [Atrocious Bot Support](t.me/Atrocious_Bot_Support)
 '''
-    update.effective_message.reply_photo("https://telegra.ph/file/16165db70a6d8c866eeed.jpg", start_string, parse_mode=ParseMode.MARKDOWN)
+    update.effective_message.reply_photo("https://telegra.ph/file/e183c440fa21cc3866d1f.png", start_string, parse_mode=ParseMode.MARKDOWN)
         
 def ping(update, context):
         sendMarkup(
